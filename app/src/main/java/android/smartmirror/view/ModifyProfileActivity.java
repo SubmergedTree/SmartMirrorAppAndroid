6c5ca122b9765735bb3dc28ed2ea24762e890827
Jannik <jannmann1@web.de>
2018-03-10 13:18:03 +0100

    added fragments

--- app/src/main/java/android/smartmirror/view/ModifyProfileActivity.java
+++ app/src/main/java/android/smartmirror/view/ModifyProfileActivity.java
@@ -1,8 +1,37 @@
 package android.smartmirror.view;
 
-/**
- * Created by jannik on 04.03.18.
- */
+import android.os.Bundle;
+import android.smartmirror.R;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class ModifyProfileActivity {
+public class ModifyProfileActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.modify_layout:
+                    return true;
+                case R.id.learning:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_profile);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
 }
